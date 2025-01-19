11bd489bebfd6ec087a2e9ab0a7ea61968884f7c
hikiko999 <132985874+hikiko999@users.noreply.github.com>
2025-01-19 01:53:40 -0600

    Q7:2 Finish with Ubuntu systemd workaround
    Used rc.d and service in playbook to enable and start apache2.
    
    Used default ubuntu user.

--- devops-challenges/code/q7/solution.sh
+++ devops-challenges/code/q7/solution.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-docker run -d --rm -p 2222:22 hikiko999/ubuntu-ssh:v1.0
+echo "Building Docker image --->"
+docker build -t mysolution .
+echo "Creating SSH Key Pair --->"
+mkdir ./.ssh
+ssh-keygen -t rsa -b 4096 -f ./.ssh/id_rsa -N ""
+chmod 600 ./.ssh/id_rsa ./.ssh/id_rsa.pub
+echo "Running Docker container --->"
+docker run --rm -d -v ./.ssh/id_rsa.pub:/home/ubuntu/.ssh/authorized_keys -p 8888:80 -p 2222:22 hikiko999/ubuntu-ssh:1.0
+
+# If hosts key checking error
+# ssh-keygen -f "/home/suadmin/.ssh/known_hosts" -R "[127.0.0.1]:2222"
+echo "Running Ansible --->"
+ansible-playbook playbooks/solution.yml
+echo "Testing server --->"
+curl localhost:8888
+echo
