094d97f57f85ae886daf4714ebb933cba1a004ef
hikiko999 <132985874+hikiko999@users.noreply.github.com>
2025-01-31 05:06:14 -0600

    Q10:5.1 Fix compute/public_dns_map output

--- devops-challenges/code/q10/modules/compute/outputs.tf
+++ devops-challenges/code/q10/modules/compute/outputs.tf
@@ -10,6 +10,11 @@ output ec2_public_dns {
     value = aws_instance.ec2[*].public_dns
 }
 
+# output "ec2_public_dns_map" {
+#   value = { for k, v in aws_instance.ec2 : k => v.public_dns }
+# }
+# { "0" = public_dns }
+
 output "ec2_public_dns_map" {
-  value = { for k, v in aws_instance.ec2 : k => v.public_dns }
+  value = { for instance in aws_instance.ec2 : instance.tags["Name"] => instance.public_dns }
 }
